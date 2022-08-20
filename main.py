38161c9e33fd8964b2766de5abab3adceedf3f19
Zukhulento <luis.matus.jr@gmail.com>
2022-08-20 12:25:16 -0600

    Update 1.1
    comentarios y nuevas respuestas

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import re
 
 
 def get_response(user_input):
-    split_message = re.split(r'\s|[,:;.?¿!-_]\s*',user_input.lower())
+    split_message = re.split(r'\s|[,:;.?¿!-_]\s*',user_input.lower()) #Se separa el mensaje del usuario y se eliminan símbolos
     response = check_all_messages(split_message)
     return response
-
+    #Se definirá la función para calcular a través de probabilidades el mejor mensaje
 def message_probability(user_message,recognized_words,single_response=False,required_word=[] ):
-    #en la definición anterior se pasan los datos para calcular qué mensaje mostrará
     message_certanly = 0
     has_requiered_words = True
     #Se procede a iterar las palabras del mensaje
@@ -35,7 +34,7 @@ def check_all_messages(message):
             highest_prob[bot_response] = message_probability(message,list_of_words, single_response,required_words)
         #casos de respuesta
         response('Hola',['hola','ola','hi','buenas','saludos'],single_response = True)
-        response('Estoy bien y tu?',['como','estas','va','vas','sientes'],required_words=['como'])
+        response('Estoy bien y tu?',['como','estas','va','vas','sientes','cómo'],single_response=True) #required_words=['como'] esto es para palabras obligadas
         response('Genial!, en qué te puedo ayudar?',['bien','excelente'],single_response = True)
         response('Oh no!, en qué te puedo ayudar?',['mal','horrible','terrible'],single_response = True)
         response('Estamos ubicados en Managua, Nicaragua',['ubicados','direccion','ubicado','lugar'],single_response=True)
@@ -48,14 +47,17 @@ def check_all_messages(message):
         ['gorras','gorra','sombrero','cabeza','accesorio','accesorios'],single_response = True)
         response('Puedes encontrar camisas en el apartado de productos y utilizando el filtro o con el buscador! :)',
         ['camisas','camisa','camiseta','camisetas','polo','polos','manga larga','manga'],single_response = True)
-        
+        response('Me llamo Codenaut Bot :) Soy una IA creada por Codenaut',['quién','quien','eres','ia','cómo','llamas','nombre','cual'],single_response=True)
+        response('Somos una tienda de ropa comprometida con la calidad y experiencia hacia nuestros clientes, somos Beachy!, si quieres saber más por favor dirigiete a la pestaña de Quienes somos?',
+        ['quienes','son','beachy','objetivo'],single_response=True)
         best_match = max(highest_prob, key=highest_prob.get)
         #Hprint(highest_prob)
         return unknown() if highest_prob[best_match] < 1 else best_match
 def unknown():
+    #Este es un caso para cuando no encuentra qué contestar
     response = ['Puedes decirlo de nuevo?','No estoy seguro de lo que quieres :(','No conozco la respuesta :('][random.randrange(3)]
     return response
     
 while True:
     
-    print("Bot : "+ get_response(input('You :')))
+    print("Bot :"+ get_response(input('You :')))
